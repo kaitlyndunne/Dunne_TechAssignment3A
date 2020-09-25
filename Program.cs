4cae157629e5d27200ed973c90d0fa0596192e0a
Kaitlyn Dunne <kdunne@usf.edu>
2020-09-24 20:11:59 -0400

    update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+    Author: Kaitlyn Dunne
+    Date: 09-24-2020
+    Comments: Demonstrates the use of iterative methods after getting input from users.
+*/
+
+using System;
 
 namespace Dunne_TechAssignment3A
 {
@@ -6,7 +12,25 @@ namespace Dunne_TechAssignment3A
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Ask for the user input
+            Console.WriteLine("Please enter your name");
+
+            try 
+            {
+                //Variable will gather data from user input
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Hello " + name.ToString() + " - Go Bucs!");
+                Console.WriteLine("Press any key to exit the program..");
+                Console.ReadKey(true);
+            }
+
+            catch
+            {
+                Console.WriteLine("Please enter your name and try running the program again...");
+                Console.WriteLine("Press any key to exit the program...");
+                Console.ReadKey(true);
+            }
         }
     }
 }
